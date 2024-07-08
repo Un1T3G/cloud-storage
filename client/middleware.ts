c4860793f49b0c825c28bd619b8aada32b2a948b
Un1T3G <un1t3gx@gmail.com>
2024-07-08 23:14:06 +0500

    update client api config

--- client/middleware.ts
+++ client/middleware.ts
@@ -6,7 +6,33 @@ const redirectToAuth = (request: NextRequest) => {
   return NextResponse.redirect(new URL('/auth/sign-in', request.url))
 }
 
+const appendHeadersConfigToRequest = (request: NextRequest) => {
+  const options = [
+    {
+      key: 'Access-Control-Allow-Credentials',
+      value: 'true',
+    },
+    {
+      key: 'Access-Control-Allow-Origin',
+      value: '*',
+    },
+    {
+      key: 'Access-Control-Allow-Methods',
+      value: 'GET,DELETE,PATCH,POST,PUT',
+    },
+    {
+      key: 'Access-Control-Allow-Headers',
+      value:
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    },
+  ]
+
+  options.forEach(({ key, value }) => request.headers.append(key, value))
+}
+
 export function middleware(request: NextRequest) {
+  appendHeadersConfigToRequest(request)
+
   const refreshToken = request.cookies.get(REFRESH_TOKEN_KEY)
 
   if (!refreshToken) {
